0b73907f1e5e382169bb749c00dc49908eb79d55
shcalbers <34623185+shcalbers@users.noreply.github.com>
2020-08-06 13:42:05 +0200

    Setup project for performance analysis

---
+++ reference/rocket.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace Tmpl8
+{
+
+class Rocket
+{
+  public:
+    Rocket(vec2 position, vec2 direction, float collision_radius, allignments allignment, Sprite* rocket_sprite);
+    ~Rocket();
+
+    void Tick();
+    void Draw(Surface* screen);
+
+    bool Intersects(vec2 position_other, float radius_other) const;
+
+    vec2 position;
+    vec2 speed;
+
+    float collision_radius;
+
+    bool active;
+
+    allignments allignment;
+
+    int current_frame;
+    Sprite* rocket_sprite;
+};
+
+} // namespace Tmpl8
